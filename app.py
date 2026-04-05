f6aaa93b1209542b6229f5d8ef92590f8ef1ba14
rithvikakella1 <ra684@cornell.edu>
2026-04-05 19:16:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if os.path.isdir("static"):
 
 @app.get("/", response_class=HTMLResponse)
 async def serve_landing():
-    with open("landing.html", "r", encoding="utf-8") as f:
+    with open("index.html", "r", encoding="utf-8") as f:
         return f.read()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
